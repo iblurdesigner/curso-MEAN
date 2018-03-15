096143befe136d140e651204d28e51306103396a
iblurdesigner <nevomen@gmail.com>
2018-03-15 15:41:17 -0500

    Agregando una ruta para crear una pregunta en el Backend

--- server/routes/question.js
+++ server/routes/question.js
@@ -23,6 +23,26 @@ const questions = new Array(10).fill(question)
 app.get('/', (req, res) => res.status(200).json(questions))
 
 // /api/questions/:id
-app.get('/:id', (req, res) => res.status(200).json(question))
+app.get('/:id', (req, res) => {
+  const { id } = req.params
+  const q = questions.find(({ _id }) => _id === +id)
+  res.status(200).json(q)
+})
+
+// POST/api/questions
+app.post('/', (req, res) => {
+  const question = req.body
+  question._id = +new Date()
+  question.user = {
+    email: 'david@mp.com',
+    password: '12345',
+    firstName: 'David',
+    lastName: 'Flores'
+  }
+  question.createdAt = new Date()
+  question.answers = []
+  questions.push(question)
+  res.status(201).json(question)
+})
 
 export default app
