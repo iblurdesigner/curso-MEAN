2e8d51071fc6f11eb9faefb76286ab5628eedf2a
iblurdesigner <nevomen@gmail.com>
2018-03-26 16:30:08 -0500

    Encriptando la contraseña del usuario

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,13 +3,17 @@ import Debug from 'debug'
 import jwt from 'jsonwebtoken'
 import { secret } from '../config'
 import { User } from '../models'
+import {
+  hashSync as hash,
+  compareSync as comparePasswords
+} from 'bcryptjs'
 
 const app = express.Router()
 const debug = new Debug('platzi-overflow:auth')
 
-function comparePasswords (providedPassword, userPassword) {
-  return providedPassword === userPassword
-}
+// function comparePasswords (providedPassword, userPassword) {
+//   return providedPassword === userPassword
+// }
 
 app.post('/signin', async (req, res, next) => {
   const { email, password } = req.body
@@ -52,7 +56,7 @@ app.post('/signup', async (req, res) => {
     firstName,
     lastName,
     email,
-    password
+    password: hash(password, 10)
   })
   debug(`Creating new user: ${user}`)
   // users.push(user)
